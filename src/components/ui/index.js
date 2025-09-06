78bc4fcee70af651fa56dd7681209dad3b6e12d7
dyaogo <167915166+dyaogo@users.noreply.github.com>
2025-09-06 20:11:20 +0000

    Create index.js

---
+++ src/components/ui/index.js
@@ -0,0 +1,18 @@
+// src/components/ui/index.js
+
+// Exports principaux
+export { default as Button } from './Button';
+export { default as Input } from './Input';
+export { default as Modal } from './Modal';
+export { default as Card } from './Card';
+export { default as Badge } from './Badge';
+
+// Exports optionnels (créer selon vos besoins)
+// export { default as LoadingSpinner } from './LoadingSpinner';
+// export { default as Toast } from './Toast';
+// export { default as Tooltip } from './Tooltip';
+// export { default as NumberInput } from './NumberInput';
+// export { default as SearchInput } from './SearchInput';
+
+// Permet d'importer ainsi :
+// import { Button, Input, Modal } from '../components/ui';
