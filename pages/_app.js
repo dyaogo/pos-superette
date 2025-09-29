33576a8f2f8b41bf92baad200d9791ac13acb25b
dyaogo <167915166+dyaogo@users.noreply.github.com>
2025-09-29 15:31:26 +0000

    fix: Simplify index page and ensure AuthProvider wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,27 @@
 import '../src/index.css';
 import { AppProvider } from '../src/contexts/AppContext';
+import { AuthProvider } from '../src/contexts/AuthContext';
 import Layout from '../components/Layout';
 import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-  
-  // Liste des pages qui N'ONT PAS besoin du menu latéral
-  const noLayout = ['/', '/login', '/test', '/simple', '/test-products'];
-  
-  // Vérifier si on doit afficher le Layout
+  const noLayout = ['/', '/login'];
   const shouldUseLayout = !noLayout.includes(router.pathname);
 
-  // Si la page a besoin du Layout (menu latéral)
-  if (shouldUseLayout) {
-    return (
-      <AppProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </AppProvider>
-    );
-  }
-
-  // Sinon, afficher la page sans Layout
+  // Toujours wrapper avec AuthProvider EN PREMIER
   return (
-    <AppProvider>
-      <Component {...pageProps} />
-    </AppProvider>
+    <AuthProvider>
+      <AppProvider>
+        {shouldUseLayout ? (
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        ) : (
+          <Component {...pageProps} />
+        )}
+      </AppProvider>
+    </AuthProvider>
   );
 }
 
