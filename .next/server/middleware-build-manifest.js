2067afce2835b9449a442e8c0f14cf0dd9585fa5
Denis Yaogo <denisyaogo@MacBook-Pro-de-Denis.local>
2025-10-20 17:52:55 +0000

    feat: Add User and ActivityLog models with authentication

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -20,11 +20,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
-    "/consolidated-dashboard": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/consolidated-dashboard.js"
-    ],
     "/users": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
